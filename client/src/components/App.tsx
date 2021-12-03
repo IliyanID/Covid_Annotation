23c4d08dc467e5e8aea1880e145bacb64a1f1ee3
Iliyan Dimitrov <18351905+IliyanID@users.noreply.github.com>
2021-12-03 14:37:18 -0700

    Restructured app

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../static/icons/logo.svg';
+import '../static/css/App.css';
 
 function App() {
   return (
