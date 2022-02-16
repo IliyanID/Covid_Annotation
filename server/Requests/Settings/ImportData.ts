59fd8d8d83f772d01886fad49dc0fc31ade13974
iliyanid <dev.iliyan.dimitrov@gmail.com>
2022-02-15 20:55:54 -0700

    f

--- server/Requests/Settings/ImportData.ts
+++ server/Requests/Settings/ImportData.ts
@@ -31,6 +31,7 @@ export const importDataRequests = (app:Express) =>{
         //console.log(req.body)
         const parsedObj = csvToJson(req.body)
             validateResponse({body:parsedObj} as Request,res,()=>{
+                console.log(parsedObj)
                 database.import_data(parsedObj).then(response=>{
                     if(database.error_state)
                         return error()
